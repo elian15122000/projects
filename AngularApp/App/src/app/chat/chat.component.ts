840b82097a211ca83184dbb07d5e5bd6265c7f95
Elian <elian.yildirim@web.de>
2020-12-13 04:11:47 +0100

    chat app

---
+++ AngularApp/App/src/app/chat/chat.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { WebSocketService } from '../services/web-socket.service';
+
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.scss']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+
+  constructor(public webSocketService: WebSocketService) {
+
+   }
+
+  ngOnDestroy(): void {
+    this.webSocketService.closeWebSocket();
+  }
+
+  ngOnInit(): void {
+    this.webSocketService.openWebSocket();
+  }
+
+  sendMessage(sendForm: NgForm) {
+    const chatMessageDto = new ChatMessageDto(sendForm.value.user, sendForm.value.message);
+    this.webSocketService.sendMessage(chatMessageDto);
+    sendForm.controls.message.reset();
+  }
+
+}
